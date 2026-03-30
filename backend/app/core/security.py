78e28d17ca5cb6925ae7cec1089afbabe1ba2c72
Nico Castro <castroconi101@gmail.com>
2026-03-30 12:57:14 -0400

    CREATED backend, and user and auth routes

---
+++ backend/app/core/security.py
@@ -0,0 +1,28 @@
+from datetime import datetime, timedelta, timezone
+from jose import jwt, JWTError
+from pwdlib import PasswordHash
+from app.core.config import settings
+
+SECRET_KEY = settings.SECRET_KEY
+ALGORITHM = settings.ALGORITHM
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
+
+password_hash = PasswordHash.recommended()
+
+def hash_password(password: str) -> str:
+    return password_hash.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return password_hash.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KET, algorithms=[ALGORITHM])
+    except JWTError:
+        return None
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,27 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.core.database import Base, engine
+from app.routes.auth import router as auth_router
+from app.deps import get_current_user
+from app.schemas.user import UserResponse
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["httpe://localhost:5173"],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.include_router(auth_router)
 
 @app.get("/api/health")
 def health():
     return {"status": "OK"}
+
+
